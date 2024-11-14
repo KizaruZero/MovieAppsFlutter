7f90674e7174c221c2a10a4707b665312b1d05e7
Ardya Pusaka <143238697+KizaruZero@users.noreply.github.com>
2024-11-14 15:34:22 +0700

    refactor: Add search functionality to NowPlayingScreenD

--- lib/screens/movies/now_playing_screen.dart
+++ lib/screens/movies/now_playing_screen.dart
@@ -3,28 +3,91 @@ import '../../services/tmdb_service.dart';
 import '../../widgets/movie_grid.dart';
 import '../../models/movie.dart';
 
-class NowPlayingScreen extends StatelessWidget {
-  final TMDBService _tmdbService = TMDBService();
+class NowPlayingScreen extends StatefulWidget {
+  const NowPlayingScreen({super.key});
 
-  NowPlayingScreen({super.key});
+  @override
+  State<NowPlayingScreen> createState() => _NowPlayingScreenState();
+}
+
+class _NowPlayingScreenState extends State<NowPlayingScreen> {
+  final TMDBService _tmdbService = TMDBService();
+  final TextEditingController _searchController = TextEditingController();
+  bool _isSearching = false;
+  // ignore: unused_field
+  String _searchQuery = '';
+  Future<List<Movie>>? _moviesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _moviesFuture = _tmdbService.getMovies('/movie/now_playing');
+  }
+
+  Future<void> _performSearch(String query) async {
+    setState(() {
+      _searchQuery = query;
+      _moviesFuture = query.isEmpty
+          ? _tmdbService.getMovies('/movie/now_playing')
+          : _tmdbService.searchMovies(query);
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Kizaru Movie'),
+        title: _isSearching ? _buildSearchField() : const Text('Kizaru Movie'),
+        actions: [
+          IconButton(
+            icon: Icon(_isSearching ? Icons.close : Icons.search),
+            onPressed: () {
+              setState(() {
+                _isSearching = !_isSearching;
+                if (!_isSearching) {
+                  _searchController.clear();
+                  _performSearch('');
+                }
+              });
+            },
+          ),
+        ],
       ),
       body: FutureBuilder<List<Movie>>(
-        future: _tmdbService.getMovies('/movie/now_playing'),
+        future: _moviesFuture,
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return MovieGrid(movies: snapshot.data!);
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No movies found'));
           }
-          return const Center(child: CircularProgressIndicator());
+          return MovieGrid(movies: snapshot.data!);
         },
       ),
+    );
+  }
+
+  Widget _buildSearchField() {
+    return TextField(
+      controller: _searchController,
+      autofocus: true,
+      decoration: const InputDecoration(
+        hintText: 'Search movies...',
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: Colors.white70),
+      ),
+      style: const TextStyle(color: Colors.white),
+      onChanged: (query) {
+        _performSearch(query);
+      },
     );
   }
 }
--- lib/services/tmdb_service.dart
+++ lib/services/tmdb_service.dart
@@ -23,6 +23,27 @@ class TMDBService {
     }
   }
 
+  Future<List<Movie>> searchMovies(String query) async {
+    if (query.isEmpty) return [];
+
+    final response = await http.get(
+      Uri.parse(
+          '${ApiConstants.baseUrl}/search/movie?api_key=${ApiConstants.apiKey}&query=$query'),
+      headers: {
+        'Authorization': 'Bearer ${ApiConstants.bearerToken}',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return (data['results'] as List)
+          .map((movie) => Movie.fromJson(movie))
+          .toList();
+    } else {
+      throw Exception('Failed to search movies');
+    }
+  }
+
   Future<void> addToFavorites(int movieId) async {
     final response = await http.post(
       Uri.parse('${ApiConstants.baseUrl}/account/{account_id}/favorite'),
